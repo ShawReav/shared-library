b397cc2ade1446cf75606a621fd762bc676df2d1
ShawReav <97753163+ShawReav@users.noreply.github.com>
2022-05-30 19:07:38 +0530

    Update authorize.groovy

--- vars/authorize.groovy
+++ vars/authorize.groovy
@@ -1,4 +1,4 @@
-def call( toolbelt,String SF_INSTANCE_URL,String SF_CONSUMER_KEY, String SF_USERNAME){
+def call(){
       			if (isUnix()) 
 				{
                 		rc = sh "${toolbelt}sfdx auth:jwt:grant --instanceurl ${SF_INSTANCE_URL} --clientid ${SF_CONSUMER_KEY} --jwtkeyfile ${server_key_file} --username ${SF_USERNAME} --setalias UAT"
