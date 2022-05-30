159e34b0375df5f36ba34dc5ea772260d8140f83
ShawReav <97753163+ShawReav@users.noreply.github.com>
2022-05-30 18:37:46 +0530

    Create pkgValidation.groovy

---
+++ vars/pkgValidation.groovy
@@ -0,0 +1,29 @@
+def call(toolbelt,SF_DELTA_FOLDER,DEPLOYDIR,String SF_USERNAME,TEST_LEVEL)
+{
+  if (DEPLOYMENT_TYPE == 'DELTA')
+            		{
+            			if (isUnix()) 
+				{
+                			rc = sh "${toolbelt}sfdx force:source:deploy -c -p ${SF_DELTA_FOLDER}/${DEPLOYDIR} -u ${SF_USERNAME} -w 500 -l ${TEST_LEVEL}"
+            			}
+				else	
+				{
+	         			rc = command "${toolbelt}sfdx force:source:deploy -c -p ${SF_DELTA_FOLDER}/${DEPLOYDIR} -u ${SF_USERNAME} -w 500 -l ${TEST_LEVEL}"
+            			}
+            		}
+            		else
+            		{
+            			if (isUnix()) 
+				{
+                			rc = sh "${toolbelt}sfdx force:source:deploy -c -p ${DEPLOYDIR} -u ${SF_USERNAME} -w 500 -l ${TEST_LEVEL}"
+            			}
+				else	
+				{
+	         			rc = command "${toolbelt}sfdx force:source:deploy -c -p ${DEPLOYDIR} -u ${SF_USERNAME} -w 500 -l ${TEST_LEVEL}"
+            			}
+            		}
+		    	if (rc != 0) 
+				{
+				error 'Component Validation Failed.'
+		    		}
+}
