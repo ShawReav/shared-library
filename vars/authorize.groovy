88c745b55d60f2ea5ab30e47abb50c2ff5bf4ef1
ShawReav <97753163+ShawReav@users.noreply.github.com>
2022-05-30 19:03:43 +0530

    Update authorize.groovy

--- vars/authorize.groovy
+++ vars/authorize.groovy
@@ -1,5 +1,4 @@
 def call( toolbelt,String SF_INSTANCE_URL,String SF_CONSUMER_KEY, String SF_USERNAME){
-  rc = command "echo y | ${toolbelt}sfdx plugins:install sfpowerkit"
       			if (isUnix()) 
 				{
                 		rc = sh "${toolbelt}sfdx auth:jwt:grant --instanceurl ${SF_INSTANCE_URL} --clientid ${SF_CONSUMER_KEY} --jwtkeyfile ${server_key_file} --username ${SF_USERNAME} --setalias UAT"
