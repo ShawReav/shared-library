97586e5b01734bd3d6c7736261d38bd09c1f9a42
ShawReav <97753163+ShawReav@users.noreply.github.com>
2022-05-30 16:12:11 +0530

    Update authorize.groovy

--- vars/authorize.groovy
+++ vars/authorize.groovy
@@ -1,6 +1,7 @@
 def SF_CONSUMER_KEY=env.SF_CONSUMER_KEY
 def SF_INSTANCE_URL = env.SF_INSTANCE_URL ?: "https://login.salesforce.com"
 def SF_USERNAME=env.SF_USERNAME
+def toolbelt = tool 'toolbelt'
 
 def call(){
   rc = command "echo y | ${toolbelt}sfdx plugins:install sfpowerkit"
