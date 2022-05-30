8bf1356c3c344a1359a3c70ccea2618ca3d20bbc
ShawReav <97753163+ShawReav@users.noreply.github.com>
2022-05-30 15:17:18 +0530

    Create authorize.groovy

---
+++ vars/authorize.groovy
@@ -0,0 +1,20 @@
+def SF_CONSUMER_KEY=env.SF_CONSUMER_KEY
+def SF_INSTANCE_URL = env.SF_INSTANCE_URL ?: "https://login.salesforce.com"
+def SF_USERNAME=env.SF_USERNAME
+
+def call(){
+  rc = command "echo y | ${toolbelt}sfdx plugins:install sfpowerkit"
+      			if (isUnix()) 
+				{
+                		rc = sh "${toolbelt}sfdx auth:jwt:grant --instanceurl ${SF_INSTANCE_URL} --clientid ${SF_CONSUMER_KEY} --jwtkeyfile ${server_key_file} --username ${SF_USERNAME} --setalias UAT"
+            			}
+			else	
+				{
+	         		rc = command "${toolbelt}sfdx auth:jwt:grant --instanceurl ${SF_INSTANCE_URL} --clientid ${SF_CONSUMER_KEY} --jwtkeyfile ${server_key_file} --username ${SF_USERNAME} --setalias UAT"
+            			}
+		    	if (rc != 0) 
+				{
+				error 'Salesforce org authorization failed.'
+		    		}
+
+}
